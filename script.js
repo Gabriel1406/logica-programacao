bb75d1617c1abbfe33719d0704225ba4ffb5b893
Gabriel Ribeiro <142447834+Gabriel1406@users.noreply.github.com>
2023-10-05 19:43:00 +0000

    feat: javaScript

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+function mediaAluno() {
+
 // Lógica de Programação 
 // Problema: obter 3 notas de um aluno, calcular a média,
 // exibir a média e exibir a situação do aluno: aprovado,
@@ -7,19 +9,27 @@ console.log('Inicio do programa');
 
 // declaração das variáveis
 
-const nota1 = parseInt(prompt('Digite a nota 1:'));
-const nota2 = parseInt(prompt('Digite a nota 2:'));
-const nota3 = parseInt(prompt('Digite a nota 3:'));
+const nota1 = parseInt(document.getElementById('nota1').value);
+const nota2 = parseInt(document.getElementById('nota1').value);
+const nota3 = parseInt(document.getElementById('nota1').value);
 
 // cálculo de média
 const media = (nota1 + nota2 + nota3) / 3;
 
 console.log('A nota 1 é:', nota1);
-
 console.log('A nota 2 é:', nota2);
-
 console.log('A nota 3 é:', nota3);
 
-console.log('A média do aluno é:', media);
+document.getElementById('resultado').innerHTML = 'A média do aluno é:' + media;
+
+if(media >= 7) {
+    document.getElementById('resultado').innerHTML += 'Situação: APROVADO';
+} else if (media >= 5) {
+    document.getElementById('resultado').innerHTML += 'Situação: EXAME';
+} else {
+    document.getElementById('resultado').innerHTML += 'Situação: REPROVADO';
+}
 
 console.log('Fim do programa');
+
+}
